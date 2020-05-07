99ee32002e0b960d87472d4d3a0ced6dced1eb25
Max Lamb <lordmaxlamb@gmail.com>
2020-05-07 16:40:35 -0700

    initial shit

---
+++ data/drop-tables.js
@@ -0,0 +1,24 @@
+const client = require('../lib/client');
+
+run();
+
+async function run() {
+  await client.connect();
+
+  try {
+    
+    await client.query(`
+            DROP TABLE IF EXISTS users CASCADE;
+            DROP TABLE IF EXISTS inhumans;
+        `);
+
+    console.log('drop tables complete');
+  }
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    client.end();
+  }
+    
+}
---
+++ data/inhumans.js
@@ -0,0 +1,47 @@
+module.exports = [
+  {
+    name: 'Black Bolt',
+    cool_factor: 10,
+    power: 'Super Voice Shit'
+  },
+  {
+    name: 'Medusa',
+    cool_factor: 7,
+    power: 'Hair Manipulation Shit'
+  },
+  {
+    name: 'Karnak The Shatterer',
+    cool_factor: 9,
+    power: 'Superior Martial Arts Shit'
+  },
+  {
+    name: 'Gorgon',
+    cool_factor: 5,
+    power: 'Seismic Shit'
+  },
+  {
+    name: 'Triton',
+    cool_factor: 6,
+    power: 'Water Breathing and Shit'
+  },
+  {
+    name: 'Crystal',
+    cool_factor: 8,
+    power: 'Element Manipulation Shit'
+  },
+  {
+    name: 'Lockjaw',
+    cool_factor: 200,
+    power: 'Teleporty Dog Shit'
+  },
+  {
+    name: 'Maximus The Mad',
+    cool_factor: 1,
+    power: 'Evil Genius Shit'
+  },
+  {
+    name: 'The Unspoken',
+    cool_factor: 1,
+    power: 'Pretty Much Whatever Shit'
+  }
+];
